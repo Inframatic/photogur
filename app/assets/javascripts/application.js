5f34d034e63ffac9aa83c30dabf4267160925bb3
Inframatic <isteffy@hotmail.com>
2013-07-07 16:20:54 -0400

    resizing and centering

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,21 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+// $(window).load(function () {
+// var $window = $(window),
+//     $main = $('.mainIframe');
+//     $blurred = $('.blurred');
+// var vague = $blurred.find('iframe').Vague({
+//   intensity:5 //blur intensity
+// });
+// vague.blur();
+
+// var scrollIframe = function () {
+//   $blurred.find('iframe').css({
+//     top: -$blurred.offset().top
+//   });
+// };
+// $window.on('scroll',scrollIframe);
+
+// scrollIframe();
+// });
